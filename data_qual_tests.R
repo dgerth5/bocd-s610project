640235d29e0eab1e537abcacdea7e8894e258b7a
dgerth5 <davidgerth5@gmail.com>
2024-12-17 22:43:54 -0500

    make test_that tests

--- data_qual_tests.R
+++ data_qual_tests.R
@@ -1,9 +1,28 @@
-#### check to make sure data is numeric and has no NA's ####
+library(testthat)
+source("bayesian-online-changepoint-script.R")
 
-d1 <- 1:5
-d2 <- c(1:5,"d")
-d3 <- c(1:5, NA)
+test_that("No errors",{
+  d1 <- 1:5
+  b1 <- bocd(d1)
+  
+  expect_no_error(b1, message = "Data is numeric, no error expected")
+  
+})
 
-b1 <- bocd(d1)
-b2 <- bocd(d2) # error as expected
-b3 <- bocd(d3) # error as expected
+test_that("Should throw non-numeric error",{
+  d2 <- c(1:5,"d")
+  
+  expect_error(bocd(d2),
+               regexp = "Non-numeric argument to mathematical function",
+               info = "Last element is not numeric, expect error")
+               
+})
+
+test_that("Should throw NA error",{
+  d3 <- c(1:5, NA)
+  b3 <- bocd(d3)
+  expect_true(b3 == "Remove NA's",
+              info = "NA is data passed through")
+  
+})
+
